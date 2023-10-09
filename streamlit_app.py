6d01f4db2c2eb601c6544083482bf0008c62b358
Natasha3101 <65615063+Natasha3101@users.noreply.github.com>
2023-10-09 11:19:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,9 @@ import pandas
 # findspark.init()
 data = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-
+fruits = data.select('Fruit')
 # multi-select
-streamlit.multiselect("Pick some fruits",list(data))
+streamlit.multiselect("Pick some fruits",list(fruits))
 
 streamlit.dataframe(data)
 
