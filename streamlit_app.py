5aa3dd1be2d9ee02a4e7d709f97bf9f09ba367aa
Natasha3101 <65615063+Natasha3101@users.noreply.github.com>
2023-10-09 10:33:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,4 @@ streamlit.header("Breakfast Menu")
 streamlit.text("Omega 3  & Blueberry Oatmeal")
 streamlit.text("Kale, Spinach & Rocket Smoothie")
 
-streamlt.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Hard-Boiled Free-Range Egg')
