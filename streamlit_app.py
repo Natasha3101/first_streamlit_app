3d82d3b61bc024ef58f5ce7cc8dfa7f32b5e6ca3
Natasha3101 <65615063+Natasha3101@users.noreply.github.com>
2023-10-09 10:51:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit
+import pyspark
+import findspark
+from pyspark.sql import SparkSession
 
+spark = SparkSession.builder.appName("snow01").getOrCreate()
+
+data = spark.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(data)
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header("Breakfast Menu")
