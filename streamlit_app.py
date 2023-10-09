a8d438906f4c78e8bb26e1e0ed9bf76f308950c9
Natasha3101 <65615063+Natasha3101@users.noreply.github.com>
2023-10-09 11:10:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,13 +7,13 @@ import  pandas as pd
 # findspark.init()
 
 # spark = SparkSession.builder.appName("snow01").getOrCreate()
-
-data = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(data)
-
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header("Breakfast Menu")
 streamlit.text("🥣Omega 3  & Blueberry Oatmeal")
 streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞 Avocado Toast")
+data = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(data)
+
+
